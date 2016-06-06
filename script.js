d07bc472fbf2b6c7d223dd505cda210d7fa916e8
eun-n <eunicena.12@gmail.com>
2016-06-06 14:54:29 -0700

    got menu to show and hide items

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function eye () {
 	document.querySelector("#coaster").src = 'assets/taeyeon1.mp4';
 	// document.querySelector("#coaster").src = 'https://www.youtube.com/v/eHir_vB1RUI';
@@ -12,12 +11,16 @@ function rain () {
 	document.querySelector("#currentvid").load();
 }
 
-function show() {
-	// document.querySelector("#ui2").src = 'assets/taei.png';
-	// document.querySelector("#ui3").src = 'assets/taeyeonrain.jpg';
-	// document.querySelector('#ui2').load();
-	// document.querySelector('#ui3').load();
-	// document.querySelector("#test").hide();
-	// document.querySelector('#test').load();
+function hide() {
+	document.getElementById('test').setAttribute('visible', false);
+	document.getElementById('test1').setAttribute('visible', false);
+	document.getElementById('menuhide').setAttribute('visible', false);
+	document.getElementById('menushow').setAttribute('visible', true);
 }
 
+function show() {
+	document.getElementById('test').setAttribute('visible', true);
+	document.getElementById('test1').setAttribute('visible', true);
+	document.getElementById('menushow').setAttribute('visible', false);
+	document.getElementById('menuhide').setAttribute('visible', true);
+}
