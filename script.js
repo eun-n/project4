57780f4202937df63c90a9942f5d43a662e05d6f
eun-n <eunicena.12@gmail.com>
2016-06-06 15:15:31 -0700

    shortened menu toggle function

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 
 function eye () {
-	document.querySelector("#coaster").src = 'assets/taeyeon1.mp4';
-	// document.querySelector("#coaster").src = 'https://www.youtube.com/v/eHir_vB1RUI';
-	
+	document.querySelector("#coaster").src = 'assets/taeyeon1.mp4';	
 	document.querySelector("#currentvid").load();
+	toggle();
 }
 
 function rain () {
 	document.querySelector("#coaster").src = 'assets/taeyeon.mp4';
 	document.querySelector("#currentvid").load();
+	toggle();
 }
 
-function hide() {
-	document.getElementById('test').setAttribute('visible', false);
-	document.getElementById('test1').setAttribute('visible', false);
-	document.getElementById('menuhide').setAttribute('visible', false);
-	document.getElementById('menushow').setAttribute('visible', true);
-}
-
-function show() {
-	document.getElementById('test').setAttribute('visible', true);
-	document.getElementById('test1').setAttribute('visible', true);
-	document.getElementById('menushow').setAttribute('visible', false);
-	document.getElementById('menuhide').setAttribute('visible', true);
+function toggle() {
+	document.getElementById('test').setAttribute('visible', !document.getElementById('test').getAttribute('visible'));
+	document.getElementById('test1').setAttribute('visible', !document.getElementById('test1').getAttribute('visible'));
 }
