493a8336e62c18bc58450770fde672e9d9d0e5fb
eun-n <eunicena.12@gmail.com>
2016-06-06 17:07:08 -0700

    changed how page loaded

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function toggle() {
 	document.getElementById('test').setAttribute('visible', !document.getElementById('test').getAttribute('visible'));
 	document.getElementById('test1').setAttribute('visible', !document.getElementById('test1').getAttribute('visible'));
 }
+
+function play1() {
+	document.getElementById('coaster').play();
+	document.getElementById('play').setAttribute('visible', !document.getElementById('play').getAttribute('visible'));
+	document.getElementById('pause').setAttribute('visible', !document.getElementById('pause').getAttribute('visible'));
+}
+
+function pause1() {
+	document.getElementById('coaster').pause();
+	document.getElementById('play').setAttribute('visible', !document.getElementById('play').getAttribute('visible'));
+	document.getElementById('pause').setAttribute('visible', !document.getElementById('pause').getAttribute('visible'));
+}
